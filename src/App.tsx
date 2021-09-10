2a8a0521664ff8d84241728cbc662ee6718020cf
Austin Pierce <Austinentpierce@gmail.com>
2021-09-10 11:14:05 -0400

    added saturation and hue sliders working lightness

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import React from 'react'
 
-
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <main>
+      <h1>Color Picker</h1>
+
+      <figure>
+        <i className="Icon fab fa-10x fa-accusoft"></i>
+      </figure>
+      <section>
+        <h2>Hue</h2>
+        <input className="hue" type="range" min="0" max="360"></input>
+        <h3>Saturation</h3>
+        <input className="saturation" type="range" min="0" max="360"></input>
+      </section>
+    </main>
+  )
 }
